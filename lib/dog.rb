31f8d668c3295beb5004b22c3e834b4faadee51e
abdishukri <abdishukrimohamed105@gmail.com>
2023-02-09 16:13:51 +0300

    completed

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,29 @@
-class Dog
+require "pry"
 
-end
+class Dog
+   
+    def name=(dog_name)
+      @name = dog_name
+    end
+  
+   
+    def name
+      @name
+    end
+  
+    def breed=(dog_breed)
+      @breed = dog_breed
+    end
+  
+    def breed
+      @breed
+    end
+  end
+  
+
+lassie = Dog.new
+
+lassie.name=("lassie")
+lassie.breed=("corgi")
+
+binding.pry
